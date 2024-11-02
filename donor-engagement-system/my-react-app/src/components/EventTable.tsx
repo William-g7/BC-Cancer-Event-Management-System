7011e01abe28c2daa977ad7c470e341a8adbf304
wangyan <wangyan@wangyandeMacBook-Air.local>
2024-11-02 12:09:11 -0700

    update style

--- donor-engagement-system/my-react-app/src/components/EventTable.tsx
+++ donor-engagement-system/my-react-app/src/components/EventTable.tsx
@@ -12,14 +12,11 @@ import ListItemText from '@mui/material/ListItemText';
 import List from '@mui/material/List';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { EventService } from '../services/eventService.ts';
+import { Event } from '../types/event.ts';
 
 
-
-const events = [
-  { id: 1, date: "4 Feb 2025", title: "Thanks Giving Party", details: "Thanksgiving celebration " },
-  { id: 2, date: "4 Feb 2025", title: "Conference in Cancer", details: "A conference discussing the latest in cancer research." },
-  { id: 3, date: "4 Feb 2025", title: "Studio", details: "An open studio event showcasing local artists." },
-];
+const eventService = new EventService();
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#fff',
@@ -36,8 +33,7 @@ const Item = styled(Paper)(({ theme }) => ({
 const EventCard: React.FC = () => {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>(); // Get the event ID from the URL
-  const event = events.find(event => event.id.toString() === id); 
-  
+  const event = eventService.getEventById(Number(id));
   
   const handleClick = () => {
     navigate(`/event/${id}/donor-selection`);
@@ -50,14 +46,14 @@ const EventCard: React.FC = () => {
       <Box sx={{width: '80%',marginTop: '70px', marginRight: '20px' }}>
         <Grid container spacing={2}>
           <Grid item sx={{marginBottom: '20px', marginLeft: '-15px'}}>
-            <Typography variant="h4" fontWeight="bold">EVENTS / {event.title}</Typography>
+            <Typography variant="h4" fontWeight="bold">EVENTS / {event.name}</Typography>
           </Grid>
 
           <Grid container spacing={45} sx={{marginBottom: '15px'}}>
             <Grid item xs={12} md={6} lg={4}>
               <Typography variant="subtitle2" color="black" fontSize={20}>Name</Typography>
               <Box sx={{ width: '300px', border: '2px solid black', padding: '8px', borderRadius: '4px' }}>
-                <Typography variant="body1" sx={{fontSize: 20}}>{event.title}</Typography>
+                <Typography variant="body1" sx={{fontSize: 20}}>{event.name}</Typography>
               </Box>
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
