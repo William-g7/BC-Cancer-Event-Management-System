91972fd8d95b8a4bf6b0a9251ea8cb4f3ff54b20
shiyuanyuanyuan <chen.shiyua@northeastern.edu>
2024-11-08 16:25:10 -0800

    insert some detailed donors to the database

--- donor-engagement-system/my-react-app/src/types/donor.ts
+++ donor-engagement-system/my-react-app/src/types/donor.ts
@@ -1,15 +1,22 @@
+import { DateTime } from 'luxon';
 export interface Donor {
     id: number;
     first_name: string;
     last_name: string;
+    pmm: string;
+    vmm: string;
+    smm: string;
     nick_name: string;
     largest_gift: number;
     total_donations: number;
-    last_gift_date: string;
-    last_gift: number;
+    largest_gift_appeal: string;
+    last_gift_appeal: string;
+    last_gift_date: DateTime;
+    last_gift_amount: number;
     address_line1: string;
     address_line2: string;
     city: string;
     phone_restrictions: string;
+    communication_restrictions: string;
     email_restrictions: string;
 }
