09461453bacc9412953c5ae51fb2da8400531df5
HaoyangChenn <zhenziyelin@gmail.com>
2024-11-12 01:13:27 -0800

    update notification page

--- donor-engagement-system/my-react-app/src/components/Notification.tsx
+++ donor-engagement-system/my-react-app/src/components/Notification.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Card, CardContent, Typography, List, ListItem } from "@mui/material";
+import { useEvents } from '../hooks/useEvents.ts';
+import { EventService } from '../services/eventService.ts';
 
 const notifications = [
   "Thanks Giving Party Selection should be finished in 2 days!",
@@ -8,14 +10,24 @@ const notifications = [
 ];
 
 const Notifications: React.FC = () => {
+  const eventService = new EventService();
+  const fetchEvents = useCallback(() => eventService.getDashboardEvents(), []);
+
+const { events, loading, error } = useEvents(fetchEvents);
+const currentTime=(times)=>{
+  const currentTime = new Date();
+  const time=new Date(times);
+  const diff = time.getTime() - currentTime.getTime();
+return Math.floor((diff/(1000*60*60*24)));  
+}
   return (
     <Card style={{border: "none", boxShadow: "none"}}>
       <CardContent>
         <Typography variant="h6">Notifications</Typography>
         <List>
-          {notifications.map((notification, index) => (
+          {events.map((notification, index) => (
             <ListItem key={index}>
-              <Typography variant="body2">{notification}</Typography>
+              <Typography variant="body2">{notification.name} Selection should be finished in {currentTime(notification.end_time)} days!</Typography>
             </ListItem>
           ))}
         </List>
--- donor-engagement-system/my-react-app/src/components/todo.tsx
+++ donor-engagement-system/my-react-app/src/components/todo.tsx
@@ -1,21 +1,28 @@
-import React from "react";
 import { Card, CardContent, Typography, List, ListItem } from "@mui/material";
+import React, { useCallback } from "react";
+import { EventService } from '../services/eventService.ts';
+import { useEvents } from '../hooks/useEvents.ts';
 
+
+const Todo: React.FC = () => {
+  const eventService = new EventService();
 const todoList = [
   "Select Donors for Thanks Giving Party Due: 11/12/2024",
   "Select Donors for Thanks Giving Party Due: 11/12/2024",
   "Select Donors for Thanks Giving Party Due: 11/12/2024",
 ];
+const fetchEvents = useCallback(() => eventService.getDashboardEvents(), []);
 
-const Todo: React.FC = () => {
+const { events, loading, error } = useEvents(fetchEvents);
+console.log(events);
   return (
     <Card style={{border: "none", boxShadow: "none"}}>
       <CardContent>
         <Typography variant="h6">To Do List</Typography>
         <List>
-          {todoList.map((todo, index) => (
+          {events.map((todo, index) => (
             <ListItem key={index}>
-              <Typography variant="body2">{todo}</Typography>
+              <Typography variant="body2">Select Donors for {todo.name} Due: {new Date(todo.start_time).toLocaleDateString()}</Typography>
             </ListItem>
           ))}
         </List>
